02855be7dabb921bdfddad7b66019bcd7cca0756
carlotajuncosa <carlota.juncosa@gmail.com>
2023-05-30 13:25:27 +0200

    server added

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const express = require('express');
 const PORT = 3000;
 const server = express();
 
+const router = express.Router();
+ server.use('/', router)
+ 
 server.listen(PORT, () => {
     console.log('Servidor a la espera de órdenes en http://localhost:${PORT}');
+})
+
+router.get('/', (request, response) => {
+    return response.send('Servidor funcionando');
 })
